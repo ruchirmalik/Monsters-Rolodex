3a95d133ee7f2cd8e7207a46407b6a6aae82a697
ruchirmalik <ruchir28@students.cs.ubc.ca>
2024-03-06 01:59:45 +0530

    Rendering and re-rendering

--- src/App.js
+++ src/App.js
@@ -6,65 +6,35 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      drivers: [
-        {
-          firstName: 'Lewis',
-          lastName: 'Hamilton',
-          id: '01'
-        },
-        {
-          firstName: 'George',
-          lastName: 'Russell',
-          id: '02'
-        },
-        {
-          firstName: 'Max',
-          lastName: 'Verstappen',
-          id: '03'
-        },
-        {
-          firstName: 'Sergio',
-          lastName: 'Perez',
-          id: '04'
-        }
-      ]
-
+      monsters: [],
     }
+
   }
 
+  // the moment your component gets placed on the DOM is when you want to make an API request, so that we can display stuff asap
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users') //it's a promise - asynchronous programming
+      .then((response) => response.json())
+      .then((users) => this.setState(() => {
+        return { monsters: users }
+      },
+        () => { console.log(this.state) }
+      ));
+  }
 
   render() {
     return (
       <div className="App">
 
-        {/* <h1> {this.state.driver1.firstName} {this.state.driver1.lastName}</h1>
-        <h1> {this.state.driver2.firstName} {this.state.driver2.lastName}</h1>
-        <h1> {this.state.driver3.firstName} {this.state.driver3.lastName}</h1>
-        <h1> {this.state.driver4.firstName} {this.state.driver4.lastName}</h1> */}
         {
-          this.state.drivers.map((driver) => {
+          this.state.monsters.map((monster) => {
             return (
-              <div key={driver.id}>
-                <h1>{driver.firstName}</h1>
+              <div key={monster.id}>
+                <h1>{monster.name}</h1>
               </div>
             );
           })
         }
-
-
-        {/* <button
-          onClick={() => {
-            this.setState(
-              () => {
-                return {
-                  drivers: { firstName: 'Asavari', lastName: 'Hari' },
-                };
-              },
-              () => { }
-            );
-          }}>
-          Change Name
-        </button> */}
 
       </div >
     );
