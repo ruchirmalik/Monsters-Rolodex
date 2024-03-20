84485b9082fa22479ffba4c31e7f9821cf9ab2d8
ruchirmalik <ruchir28@students.cs.ubc.ca>
2024-03-20 09:40:27 +0530

    Project Completed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Component } from 'react';
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 
+
 class App extends Component {
 
   constructor() {
@@ -48,6 +49,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1 className='app-title'>Monsters Rolodex</h1>
 
         <SearchBox onChangeHandler={onSearchChange} placeholder={'Search Monsters'} className={'monsters-search-box'} />
         <CardList monsters={filteredMonsters} />
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,26 +1,15 @@
 import { Component } from "react";
 import "./card-list.styles.css";
+import Card from "./../card/card.component";
 
 class CardList extends Component {
   render() {
     const { monsters } = this.props;
 
-    //console.log("render from CardList");
-    // console.log(monsters);
     return (
       <div className="card-list">
         {monsters.map((monster) => {
-          const { name, id, email } = monster;
-          return (
-            <div className="card-container" key={id}>
-              <img
-                alt={`monster ${monster.name}`}
-                src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
-              ></img>
-              <h2>{name}</h2>
-              <p>{email}</p>
-            </div>
-          );
+          return <Card monster={monster} />;
         })}
       </div>
     );
