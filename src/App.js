60457f1263b758e943c4cb9cc12dbb16ce54c12c
ruchirmalik <ruchir.malik312@gmail.com>
2024-03-26 18:28:59 +0530

    Infinite re-rendering problem with fetch()

--- src/App.js
+++ src/App.js
@@ -1,62 +1,92 @@
 import logo from './logo.svg';
 import './App.css';
-import { Component } from 'react';
+import { useState } from 'react'
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 
+const App = () => {
 
-class App extends Component {
+  const [searchField, setSearchField] = useState('');
+  const [monsters, setMonsters] = useState([])
+  //console.log(searchField)
 
-  constructor() {
-    super();
-    this.state = {
-      monsters: [],
-      searchField: ' '
-    }
+  console.log('render');
 
-  }
+  //since the users is coming from the outside through the API URL, it is a different array in memory. Hence infinite re-rendering
+  fetch('https://jsonplaceholder.typicode.com/users') //it's a promise - asynchronous programming
+    .then((response) => response.json())
+    .then((users) => setMonsters(users)
+    );
 
-  // the moment your component gets placed on the DOM is when you want to make an API request, so that we can display stuff asap
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users') //it's a promise - asynchronous programming
-      .then((response) => response.json())
-      .then((users) => this.setState(() => {
-        return { monsters: users }
-      }
-
-      ));
-  }
-
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLocaleLowerCase();
-    this.setState(
-      () => {
-        return { searchField }
-      }
-    )
+  const onSearchChange = (event) => {
+    console.log('render')
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
   };
 
-  render() {
+  const filteredMonsters = monsters.filter((monster) => {
+    return monster.name.toLocaleLowerCase().includes(searchField);
+  })
 
-    console.log('render for AppJS')
-    //destructuring - makes variables look shorter and makes things easier to read
-    const { monsters, searchField } = this.state;
-    const { onSearchChange } = this;
+  return (
+    <div className="App">
+      <h1 className='app-title'>Monsters Rolodex</h1>
 
-    const filteredMonsters = monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(searchField);
-    })
+      <SearchBox
+        onChangeHandler={onSearchChange}
+        placeholder={'Search Monsters'}
+        className={'monsters-search-box'} />
+      <CardList monsters={filteredMonsters} />
 
-    return (
-      <div className="App">
-        <h1 className='app-title'>Monsters Rolodex</h1>
-
-        <SearchBox onChangeHandler={onSearchChange} placeholder={'Search Monsters'} className={'monsters-search-box'} />
-        <CardList monsters={filteredMonsters} />
-
-      </div >
-    );
-  }
+    </div >)
 }
+
+// class App extends Component {
+
+//   constructor() {
+//     super();
+//     this.state = {
+//       monsters: [],
+//       searchField: ' '
+//     }
+
+//   }
+
+//   // the moment your component gets placed on the DOM is when you want to make an API request, so that we can display stuff asap
+//   componentDidMount() {
+// fetch('https://jsonplaceholder.typicode.com/users') //it's a promise - asynchronous programming
+//   .then((response) => response.json())
+//   .then((users) => this.setState(() => {
+//     return { monsters: users }
+//   }
+
+//       ));
+//   }
+
+//   onSearchChange = (event) => {
+//     const searchField = event.target.value.toLocaleLowerCase();
+//     this.setState(
+//       () => {
+//         return { searchField }
+//       }
+//     )
+//   };
+
+//   render() {
+
+//     console.log('render for AppJS')
+//     //destructuring - makes variables look shorter and makes things easier to read
+//     const { monsters, searchField } = this.state;
+//     const { onSearchChange } = this;
+
+//     const filteredMonsters = monsters.filter((monster) => {
+//       return monster.name.toLocaleLowerCase().includes(searchField);
+//     })
+
+//     return (
+
+//     );
+//   }
+// }
 
 export default App;
